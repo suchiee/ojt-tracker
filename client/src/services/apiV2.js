ae44cbe472abb963f61483f89010967a87a8707f
suchiee <suchitra.y.1206@gmail.com>
2026-07-27 17:17:43 +0530

    Backup before configuration refactor

--- client/src/services/apiV2.js
+++ client/src/services/apiV2.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { supabase } from './supabaseClient';
 
 const apiV2 = axios.create({
-  baseURL: 'https://internsync-api-vjil.onrender.com/api/v2',
+  baseURL: 'http://localhost:5001/api/v2',
   headers: {
     'Content-Type': 'application/json'
   }
